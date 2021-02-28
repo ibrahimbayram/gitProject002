ad9f39d03bbdd57ec026abb4f13afa953947102c
ibrahimbayram <79757271+ibrahimbayram@users.noreply.github.com>
2021-02-28 18:31:21 +0100

    Update Class1_2.java

--- src/gitProject002/Class1_2.java
+++ src/gitProject002/Class1_2.java
@@ -4,6 +4,9 @@ public class Class1_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		This changes are done on virtuel
+			//while teacher makes practice with Pyton , i work on this
+			
 
 	}
 
