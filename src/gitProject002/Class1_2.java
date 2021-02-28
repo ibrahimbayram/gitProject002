e85ada3384a42066638f58efbfc4ff5b61390fe8
Ibrahim Bayram <ibrahim33bayram@gmail.com>
2021-02-28 18:36:39 +0100

    last commit

--- src/gitProject002/Class1_2.java
+++ src/gitProject002/Class1_2.java
@@ -3,9 +3,12 @@ package gitProject002;
 public class Class1_2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		This changes are done on virtuel
+		// Hello World
+		//This changes are done on virtuel
 			//while teacher makes practice with Pyton , i work on this
+			System.out.println("Hello World");
+			
+			//Now this changes are done on Ibrahim branch.
 			
 
 	}
